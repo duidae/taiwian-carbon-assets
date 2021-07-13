9d3b65665908b28ff967fa9c94ab8457cf7efd9b
duidae <duidae@gmail.com>
2021-07-13 22:18:50 +0800

    Move css to file

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,10 +1,11 @@
 import {Component} from "react";
 import GoogleMapReact from "google-map-react";
+import "./Map.scss";
 
 export class Map extends Component {
     render() {
         return (
-            <div style={{height: "100vh", width: "100%"}}>
+            <div className="map">
                 <GoogleMapReact defaultCenter={{lat: 59.95, lng: 30.33}} defaultZoom={11}>
                 </GoogleMapReact>
             </div>
