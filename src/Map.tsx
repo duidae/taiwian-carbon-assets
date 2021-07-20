90f1a46ced21e24b4a13a17b3cde2bb79f58d643
duidae <duidae@gmail.com>
2021-07-20 15:52:28 +0800

    Enable geojson

--- src/Map.tsx
+++ src/Map.tsx
@@ -3,10 +3,27 @@ import GoogleMapReact from "google-map-react";
 import "./Map.scss";
 
 export class Map extends Component {
+    private loadGeojson = (map: any, maps: any) => {
+        map.data.setStyle({
+            fillColor: "green",
+            strokeColor: "green",
+            clickable: true
+        });
+
+        map.data.loadGeoJson(
+            "testGeojson.json"
+        );
+    };
+
     render() {
         return (
             <div className="map">
-                <GoogleMapReact defaultCenter={{lat: 24.977707, lng: 121.277557}} defaultZoom={15}>
+                <GoogleMapReact
+                    defaultCenter={{lat: 24.985892654871, lng: 121.55384976076}}
+                    defaultZoom={17}
+                    yesIWantToUseGoogleMapApiInternals
+                    onGoogleApiLoaded={({map, maps}) => this.loadGeojson(map, maps)}
+                >
                 </GoogleMapReact>
             </div>
         );
