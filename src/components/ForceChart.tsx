f351bfd2b26ce8b70a26b3f8dc18737db2e8f854
duidae <duidae@gmail.com>
2021-07-29 20:21:36 +0800

    Fix chart style issue

--- src/components/ForceChart.tsx
+++ src/components/ForceChart.tsx
@@ -1,35 +1,8 @@
 import {merge} from "lodash";
 import ReactApexChart from "react-apexcharts";
-import {useTheme, styled} from "@material-ui/core/styles";
+import {useTheme} from "@material-ui/core/styles";
 import {Card, CardHeader} from "@material-ui/core";
 import BaseOptionChart from "./BaseOptionChart";
-
-// ----------------------------------------------------------------------
-
-const CHART_HEIGHT = 392;
-const LEGEND_HEIGHT = 72;
-
-/*
-const ChartWrapperStyle = styled('div')(({ theme }) => ({
-  height: CHART_HEIGHT,
-  marginTop: theme.spacing(2),
-  '& .apexcharts-canvas svg': {
-    height: CHART_HEIGHT
-  },
-  '& .apexcharts-canvas svg,.apexcharts-canvas foreignObject': {
-    overflow: 'visible'
-  },
-  '& .apexcharts-legend': {
-    height: LEGEND_HEIGHT,
-    alignContent: 'center',
-    position: 'relative !important',
-    borderTop: `solid 1px ${theme.palette.divider}`,
-    top: `calc(${CHART_HEIGHT - LEGEND_HEIGHT}px) !important`
-  }
-}));
-*/
-
-// ----------------------------------------------------------------------
 
 const CHART_DATA = [
     {name: "光電設施", data: [80, 50, 30, 40, 100, 20]},
@@ -43,7 +16,7 @@ export default function AppCurrentSubject() {
     const chartOptions = merge(BaseOptionChart(), {
         stroke: {width: 2},
         fill: {opacity: 0.48},
-        legend: {floating: true, horizontalAlign: "center"},
+        legend: {floating: false, horizontalAlign: "center"},
         xaxis: {
             categories: ["節能", "減碳", "開發經費", "環境評估", "綜和效益", "人力"],
             labels: {
@@ -57,12 +30,7 @@ export default function AppCurrentSubject() {
     return (
         <Card>
             <CardHeader title="減碳策略" />
-            <ReactApexChart type="radar" series={CHART_DATA} options={chartOptions} height={340} />
-            {/*
-      <ChartWrapperStyle dir="ltr">
-        <ReactApexChart type="radar" series={CHART_DATA} options={chartOptions} height={340} />
-      </ChartWrapperStyle>
-      */}
+            <ReactApexChart type="radar" series={CHART_DATA} options={chartOptions}/>
         </Card>
     );
 }
