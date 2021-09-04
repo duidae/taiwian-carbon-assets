1376f357bd876e1da6b20b6f57eb2686af540fa1
duidae <duidae@gmail.com>
2021-09-04 22:23:59 +0800

    Add fps meter

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import Stats from "stats-js";
 
 ReactDOM.render(
     <React.StrictMode>
@@ -15,3 +16,24 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// TODO: dynamic import
+/*
+import("stats-js").then(({ Stats }) => {
+}).catch(err => {
+    console.log(err);
+});
+*/
+
+const stats = new Stats();
+stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
+document.body.appendChild(stats.dom);
+function animate() {
+    stats.begin();
+    // monitored code goes here
+    stats.end();
+    requestAnimationFrame(animate);
+}
+requestAnimationFrame(animate);
+stats.dom.style.right = "0";
+stats.dom.style.left = "initial";
