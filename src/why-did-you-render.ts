89d0eda3b5b441d6d87b13c8ecce9824bb866048
duidae <duidae@gmail.com>
2021-09-12 19:25:09 +0800

    Add options to why did you render

--- src/why-did-you-render.ts
+++ src/why-did-you-render.ts
@@ -4,6 +4,8 @@ import React from "react";
 if (process.env.REACT_APP_ENVIRONMENT) {
     const whyDidYouRender = require("@welldone-software/why-did-you-render");
     whyDidYouRender(React, {
+        titleColor: "green",
+        diffNameColor: "aqua",
         trackAllPureComponents: true
     });
 }
