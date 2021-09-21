e8930d02871a888fc6a9d0579fb985bb42ddfdec
duidae <duidae@gmail.com>
2021-09-21 10:15:26 +0800

    Add slider

--- src/components/CarbonSinkEstimation.tsx
+++ src/components/CarbonSinkEstimation.tsx
@@ -1,23 +1,45 @@
 import {withStyles} from "@material-ui/core/styles";
-import {Card, CardHeader} from "@material-ui/core";
+import {Card, CardHeader, Slider} from "@material-ui/core";
 
 const styles = theme => ({
     root: {
         height: "100%"
     },
-    chart: {
-        height: "calc(100% - 64px)"
-    }
 });
 
 function CarbonSink(props) {
     const classes = props.classes;
 
+    const percentages = [
+        {
+          value: 50,
+          label: '50%',
+        },
+        {
+          value: 25,
+          label: '25%',
+        },
+        {
+          value: 10,
+          label: '10%',
+        },
+    ];
+
+    function percentageText(value) {
+        return `${value}%`;
+    }
+
     return (
         <Card className={classes.root}>
             <CardHeader title="碳匯估算" />
-            <div className={classes.chart}>
-            </div>
+            <Slider
+                aria-label="Custom marks"
+                defaultValue={50}
+                getAriaValueText={percentageText}
+                step={10}
+                valueLabelDisplay="on"
+                marks={percentages}
+            />
         </Card>
     );
 }
