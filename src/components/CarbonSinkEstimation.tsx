495dfc104c2c95862b7180b6adeb135fc169a2de
duidae <duidae@gmail.com>
2021-09-23 10:25:39 +0800

    Format

--- src/components/CarbonSinkEstimation.tsx
+++ src/components/CarbonSinkEstimation.tsx
@@ -4,7 +4,7 @@ import {Card, CardHeader, Slider} from "@material-ui/core";
 const styles = theme => ({
     root: {
         height: "100%"
-    },
+    }
 });
 
 function CarbonSink(props) {
@@ -12,17 +12,17 @@ function CarbonSink(props) {
 
     const percentages = [
         {
-          value: 50,
-          label: '50%',
+            value: 50,
+            label: "50%"
         },
         {
-          value: 25,
-          label: '25%',
+            value: 25,
+            label: "25%"
         },
         {
-          value: 10,
-          label: '10%',
-        },
+            value: 10,
+            label: "10%"
+        }
     ];
 
     function percentageText(value) {
@@ -32,14 +32,7 @@ function CarbonSink(props) {
     return (
         <Card className={classes.root}>
             <CardHeader title="碳匯估算" />
-            <Slider
-                aria-label="Custom marks"
-                defaultValue={50}
-                getAriaValueText={percentageText}
-                step={10}
-                valueLabelDisplay="on"
-                marks={percentages}
-            />
+            <Slider aria-label="Custom marks" defaultValue={50} getAriaValueText={percentageText} step={10} valueLabelDisplay="on" marks={percentages} />
         </Card>
     );
 }
