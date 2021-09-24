602fe40f9888eabd123f2092874393645bc0badd
duidae <duidae@gmail.com>
2021-09-24 08:38:23 +0800

    Add formula

--- src/components/CarbonSinkEstimation.tsx
+++ src/components/CarbonSinkEstimation.tsx
@@ -37,12 +37,25 @@ function CarbonSink(props) {
         return AppStore.Instance.selectedAreaSolarPanelArea * percentage * CO2_EQ;
     };
 
+    const formulaDescription = (
+        <p>
+            台北市10平方公尺（約3坪）的屋頂可利用面積
+            <br />
+            ＝1kWp的裝置容量
+            <br />
+            ＝每年大概發電1000度電
+            <br />
+            ＝約可減下509公斤的二氧化碳當量
+        </p>
+    );
+
     return (
         <Card className={classes.root}>
             <CardHeader title="碳匯估算" />
             <Slider aria-label="Custom marks" defaultValue={50} getAriaValueText={percentageText} step={10} valueLabelDisplay="on" marks={percentages} />
             <p>預估每年效益</p>
             {`約可減下${carbonSinkValue(50)}公斤CO2當量`}
+            {"計算公式"}
         </Card>
     );
 }
