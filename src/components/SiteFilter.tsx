49950356b8a37c31a63da8c3c3b5932274d4fbf7
duidae <duidae@gmail.com>
2021-11-06 11:04:38 +0800

    Enable pics & filter

--- src/components/SiteFilter.tsx
+++ src/components/SiteFilter.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import {observer} from "mobx-react";
 import {withStyles} from "@material-ui/core/styles";
-import {Card, CardHeader} from "@material-ui/core";
+import {Card, CardHeader, FormControl, InputLabel, MenuItem, Select} from "@material-ui/core";
 
 import {AppStore} from "stores";
 
@@ -19,9 +19,24 @@ const styles = theme => ({
 
 @observer
 class siteFilter extends React.Component<any, any> {
+    private categories = ["綠資源", "光電設施", "小風機", "設備經營管理", "小水力", "生質能", "水環境", "再生農漁業", "低碳社區", "地方創生", "大學合作", "園區開發", "都市更新", "重劃區"].map(category => {
+        return <MenuItem>{category}</MenuItem>;
+    });
+
+    private handleCategoryChange = () => {};
+
     public render() {
         const classes = this.props.classes;
-        return <Card className={classes.root} variant="outlined"></Card>;
+        return (
+            <Card className={classes.root} variant="outlined">
+                <FormControl fullWidth>
+                    <InputLabel id="demo-simple-select-label">減碳方式</InputLabel>
+                    <Select labelId="demo-simple-select-label" id="demo-simple-select" value={undefined} label="減碳方式" onChange={this.handleCategoryChange}>
+                        {this.categories}
+                    </Select>
+                </FormControl>
+            </Card>
+        );
     }
 }
 
--- src/components/SitePicture.tsx
+++ src/components/SitePicture.tsx
@@ -24,7 +24,7 @@ class sitePicture extends React.Component<any, any> {
         return (
             <Card className={classes.root} variant="outlined">
                 <CardHeader title="案場景象" />
-                {false && <img src={"matching/1.jpg"} alt={"1.jpg"} />}
+                {<img height={250} src={"matching/1.jpg"} alt={"1.jpg"} />}
             </Card>
         );
     }
